64420ebda7f652d6b8acb121a45d0302d76e77f3
hantao <flagtp@163.com>
2019-11-26 15:43:45 +0800

    初始化配置文件

---
+++ src/layout/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Layout'
+
+}
+
+</script>
+<style>
+</style>
---
+++ src/settings.js
@@ -0,0 +1,12 @@
+module.exports = {
+  /**
+    * @type {String}   
+    * @description 系统标题
+    */
+  title: '山西省审计厅档案综合管理系统',
+  /**
+    * @type {String}   
+    * @description 版权
+    */
+  copyRight: ''
+}
