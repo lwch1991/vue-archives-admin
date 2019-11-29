54940dee63e1741e7b03040a12837656839449be
hantao <flagtp@163.com>
2019-11-29 10:53:15 +0800

    安装vuetify插件

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
+
+// import style
+import 'normalize.css/normalize.css'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
